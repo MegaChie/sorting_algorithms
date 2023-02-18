fe1aa6475a835ad7a64c91d1ef94464b3d1b8a3a
MegaChie <workspace.mega@gmail.com>
2023-02-18 22:17:35 +0200

    Update swapFunctions.c

--- swapFunctions.c
+++ swapFunctions.c
@@ -18,13 +18,13 @@ void swapNodes(listint_t **h, listint_t **node1, listint_t node2)
 	{
 		node2->next->prev = *node1;
 	}
-	node2->prev = (node1)->prev;
+	node2->prev = (*node1)->prev;
 	node2->next = *node1;
 	if ((*node1)->prev != NULL)
 	{
 		(*node1)->prev->next = node2;
 	}
-	else:
+	else
 	{
 		*h = node2;
 	}
