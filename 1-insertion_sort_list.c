849a1a08a065e9e13b610277137c7d4c52660aa3
MegaChie <workspace.mega@gmail.com>
2023-02-18 20:41:48 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,5 +4,20 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	
+	listint_t rounds, *invalue, *temp;
+
+	if(list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	for (rounds = (*list)->next; rounds != NULL; rounds == temp)
+	{
+		temp = rounds->next;
+		invalue = rounds->prev;
+		while(invalue != NULL && rounds->n < invalue->n)
+		{
+			swapNodes(list, &invalue, rounds);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
