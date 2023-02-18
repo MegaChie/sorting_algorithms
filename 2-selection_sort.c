e53f36bb0b1e963d5fe73893ce0b0568dadb0dba
MegaChie <workspace.mega@gmail.com>
2023-02-18 21:52:11 +0200

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * 
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *mini;
+	size_t raw, collumn;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (raw = 0; raw < size - 1; raw++)
+	{
+		mini = array + raw;
+		for (collumn = raw + 1; collumn < size; collumn++)
+		{
+			mini = (array[collumn] < *mini) ? (array + collumn) : mini;
+		}
+		if ((array + raw) != mini)
+		{
+			swapvalues(array + raw, mini);
+			print_array(array, size);
+		}
+	}
+}
