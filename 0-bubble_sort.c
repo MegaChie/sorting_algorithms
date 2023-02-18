fb841af619d2821f9b3308efb32d2f88bdeb63d6
MegaChie <workspace.mega@gmail.com>
2023-02-18 20:02:54 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,5 +4,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	
+	size_t count, len = size;
+	bool goOn = false;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	while (goOn == false)
+	{
+		goOn = true;
+		for (count = 0; count < len - 1; count++)
+		{
+			if (array[count] > array[count + 1])
+			{
+				swapValues(array + count, array + count + 1);
+				print_array(array, size);
+				goOn = false;
+			}
+		}
+		len--l
+	}
 }
