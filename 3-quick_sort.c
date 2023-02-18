c4840d56a134f7d11484bb0994fb64fbb5f44006
MegaChie <workspace.mega@gmail.com>
2023-02-18 22:07:28 +0200

    Create 3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+/**
+ * 
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	lomotoSort(array, size, 0, size - 1);
+}
+
+/**
+ * 
+ */
+int lomotoPartition(int *array, size_t size, int left, int right)
+{
+	int *center, up, down;
+
+	center = array + right;
+	for (up = down = left; down < right; down++)
+	{
+		if (array[down] < *center)
+		{
+			if (up < down)
+			{
+				swapValues(array + down, array + up);
+				print_array(array, size);
+			}
+			up++
+		}
+	}
+}
+
+/**
+ * 
+ */
+void lomutoSort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomotoPartition(array, size, left, right);
+		lomotoSort(array, size, left, part - 1);
+		lomotoSort(array, size, part + 1, right);
+	}
+}
