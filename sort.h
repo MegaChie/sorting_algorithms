dcffa69bc08bf9949960e4d051aafe411a6c5365
MegaChie <workspace.mega@gmail.com>
2023-02-18 22:17:38 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,3 +1,5 @@
+#ifndef SORTER
+#define SORTER
 /**
  * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
@@ -13,3 +15,9 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void swapValues(int *a, int *b);
+void swapNodes(listint_t **h, listint_t **node1, listint_t node2);
+#endif
