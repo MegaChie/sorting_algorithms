725babfe507ed48e8d0d7d90e94ae64baf0249b4
MegaChie <workspace.mega@gmail.com>
2023-02-19 00:30:27 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t rounds, *inValue, *temp;
+	listint_t *rounds, *inValue, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
