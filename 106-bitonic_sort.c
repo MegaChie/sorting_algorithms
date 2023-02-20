f6102c72ccfba4fbb5c35a0bdf8d38fd4aa9124d
MegaChie <workspace.mega@gmail.com>
2023-02-20 13:38:03 +0200

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -21,23 +21,21 @@ void swapValues(int *a, int *b)
  * @bits: structure variable
  * @result: charecter variable
  */
-void bitMerge(int *array, size_t size, size_t start, size_t sequance,
+void bitMerge(int *array, size_t size, size_t start, size_t seq,
 		char result)
 {
-	size_t i, hop = sequance / 2;
+	size_t i, jump = seq / 2;
 
-	if (sequance > 1)
+	if (seq > 1)
 	{
-		for (i = start; i < start + hop; i++)
+		for (i = start; i < start + jump; i++)
 		{
-			if ((result == high && array[i] > array[i + hop]) ||
-			    (result == low && array[i] < array[i + hop]))
-			{
-				swapValues(array + i, array + i + hop);
-			}
+			if ((result == high && array[i] > array[i + jump]) ||
+			    (result == low && array[i] < array[i + jump]))
+				swapValues(array + i, array + i + jump);
 		}
-		bitMerge(array, size, start, hop, result);
-		bitMerge(array, size, start + hop, hop, result);
+		bitMerge(array, size, start, jump, result);
+		bitMerge(array, size, start + jump, jump, result);
 	}
 }
 
@@ -49,21 +47,22 @@ void bitMerge(int *array, size_t size, size_t start, size_t sequance,
  * @sequance: structure variable
  * @result: charecter variable
  */
-void bitSequance(int *array, size_t size, size_t start, size_t sequance,
-	char result)
+void bitSequance(int *array, size_t size, size_t start, size_t seq, char result)
 {
-	size_t cut = sequance / 2;
+	size_t cut = seq / 2;
 	char *str = (result == high) ? "high" : "low";
 
-	if (sequance > 1)
+	if (seq > 1)
 	{
-		printf("Merging [%lu/%lu] (%s):\n", sequance, size, str);
-		print_array(array + start, sequance);
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+
 		bitSequance(array, size, start, cut, high);
 		bitSequance(array, size, start + cut, cut, low);
-		bitMerge(array, size, start, sequance, result);
-		printf("Result [%lu/%lu] (%s):\n", sequance, size, str);
-		print_array(array + start, sequance);
+		bitMerge(array, size, start, seq, result);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
 	}
 }
 /**
